86e26db3fcd063a906c9189fac6a27ba4a129011
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2023-05-23 20:53:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from game import *
 import pygame
 import math
-
+from AI import *
+from sys import exit
 # one time initialized object
 game = game()
 
@@ -29,9 +30,9 @@ def draw_board(g: game):
 	
 	for col in range(g.board.columns):
 		for row in range(g.board.rows):		
-			if g.board[row][col] == player.P1_piece:
+			if g.board.slots[row][col] == player.P1_piece:
 				pygame.draw.circle(screen, RED, (int(col*SQUARESIZE+SQUARESIZE/2), height-int(row*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-			elif g.board[row][col] == player.P2_piece: 
+			elif g.board.slots[row][col] == player.P2_piece: 
 				pygame.draw.circle(screen, YELLOW, (int(col*SQUARESIZE+SQUARESIZE/2), height-int(row*SQUARESIZE+SQUARESIZE/2)), RADIUS)
 	pygame.display.update()
 
@@ -43,60 +44,50 @@ pygame.display.update()
 # game loop
 while not game.over:
 
+# interactive GUI on the player's turn
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			sys.exit()
+			exit()
 
 		if event.type == pygame.MOUSEMOTION:
 			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
 			posx = event.pos[0]
-			if turn == PLAYER:
+			if game.currentPlayer() is player.P1_piece:
 				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
 
 		pygame.display.update()
 
+		# input action is taken from user
 		if event.type == pygame.MOUSEBUTTONDOWN:
+			# draw black box 
 			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-			#print(event.pos)
-			# Ask for Player 1 Input
-			if turn == PLAYER:
+			# Ask for Player Input
+			if game.currentPlayer() is player.P1_piece:
 				posx = event.pos[0]
 				col = int(math.floor(posx/SQUARESIZE))
 
-				if is_valid_location(board, col):
-					row = get_next_open_row(board, col)
-					drop_piece(board, row, col, PLAYER_PIECE)
-
-					if winning_move(board, PLAYER_PIECE):
+				if game.turn(col):
+					if game.winning_move():
 						label = myfont.render("Player 1 wins!!", 1, RED)
 						screen.blit(label, (40,10))
-						game_over = True
+						game.over = True
 
-					turn += 1
-					turn = turn % 2
-
-					print_board(board)
-					draw_board(board)
+					game.board.show()
+					draw_board(game)
 
 
-	# # Ask for Player 2 Input
-	if turn == AI and not game_over:				
-		col, minimax_score = minimax(board, 5, -math.inf, math.inf, True)
+	# # Non reactive AI player turn
+	if game.currentPlayer() is player.P2_piece and not game_over:				
+		col, minimax_score = minimax(game, 5, -math.inf, math.inf, True)
 
-		if is_valid_location(board, col):
-			row = get_next_open_row(board, col)
-			drop_piece(board, row, col, AI_PIECE)
-
-			if winning_move(board, AI_PIECE):
+		if game.turn(col):
+			if game.winning_move(player.P2_piece):
 				label = myfont.render("Player 2 wins!!", 1, YELLOW)
 				screen.blit(label, (40,10))
 				game_over = True
 
-			print_board(board)
-			draw_board(board)
-
-			turn += 1
-			turn = turn % 2
+			game.board.show()
+			draw_board(game)
 
 	if game_over:
 		pygame.time.wait(3000)
