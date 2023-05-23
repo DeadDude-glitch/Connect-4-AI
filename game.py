b1ef4193b24239529180a20b88e32b7911c05078
Ahmed Mamdouh <72521031+DeadDude-glitch@users.noreply.github.com>
2023-05-23 20:52:33 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,10 @@
 from board import *
 from enum import Enum
 
+# a once initialized object that represent a connect four game
+# this module should depend only on board module
+# all actions should be simplified in a function
+
 # constant player values
 class player(Enum):
     P1_piece =  1   # human
@@ -15,38 +19,41 @@ class game:
         self.board = Board()
         self.turn_counter = 0
 
-    def currentPlayer(self) -> int:
+    def currentPlayer(self) -> player:
         if self.turn_counter % 2 == 0: 
-            return 1
-        return -1
+            return player.P1_piece
+        return player.P2_piece
     
-    def turn(self, col:int=-1):
+    # let player take his turn and return if turn is taken successfully
+    def turn(self, col:int=-1) -> bool:
         if not col in range(0, self.board.columns-1): 
             print(f"Index Out Of Range (0-{self.board.columns-1})")
             return False
         self.board.drop_piece(col, self.currentPlayer())
-        if self.winning_move(self.currentPlayer): 
+        if self.winning_move(): 
             print("GAMEOVER")
         self.turn_counter += 1
         return True
 
-    def winning_move(self, piece:player) -> bool:
+    # scans the entire board for 4 consecutive similar pieces
+    def winning_move(self) -> bool:
         # Check horizontal win
+        piece = self.currentPlayer()
         for c in range(self.board.columns-3):
             for r in range(self.board.rows):
-                if (    self.board[r][c]   == piece 
-                    and self.board[r][c+1] == piece 
-                    and self.board[r][c+2] == piece 
-                    and self.board[r][c+3] == piece
+                if (    self.board.slots[r][c]   == piece 
+                    and self.board.slots[r][c+1] == piece 
+                    and self.board.slots[r][c+2] == piece 
+                    and self.board.slots[r][c+3] == piece
                 ): return True
         
         # Check vertical win 
         for c in range(self.board.columns):
             for r in range(self.board.rows-3): 
-                if (	self.board[r][c]   == piece 
-                    and self.board[r+1][c] == piece 
-                    and self.board[r+2][c] == piece 
-                    and self.board[r+3][c] == piece 
+                if (	self.board.slots[r][c]   == piece 
+                    and self.board.slots[r+1][c] == piece 
+                    and self.board.slots[r+2][c] == piece 
+                    and self.board.slots[r+3][c] == piece 
                 ): return True
         
         # Check diagonal win
@@ -54,19 +61,19 @@ class game:
         # negative slope  \
         for c in range(self.board.columns-3):
        	    for r in range(self.board.rows-3):
-               if (     self.board[r][c]     == piece 
-                    and self.board[r+1][c+1] == piece 
-                    and self.board[r+2][c+2] == piece 
-                    and self.board[r+3][c+3] == piece
+               if (     self.board.slots[r][c]     == piece 
+                    and self.board.slots[r+1][c+1] == piece 
+                    and self.board.slots[r+2][c+2] == piece 
+                    and self.board.slots[r+3][c+3] == piece
                 ): return True
 		
-  # positive slope  /
+        # positive slope  /
         for c in range(self.board.columns-3):
             for r in range(3, self.board.rows):
-                if (	self.board[r][c]     == piece 
-                    and self.board[r-1][c+1] == piece 
-                    and self.board[r-2][c+2] == piece 
-                    and self.board[r-3][c+3] == piece
+                if (	self.board.slots[r][c]     == piece 
+                    and self.board.slots[r-1][c+1] == piece 
+                    and self.board.slots[r-2][c+2] == piece 
+                    and self.board.slots[r-3][c+3] == piece
                 ): return True
 
 
